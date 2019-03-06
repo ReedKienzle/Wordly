0d362604ac7f01c96d91d45a2ff6fe4ad33629dd
Lam Nguyen <oregon597@gmail.com>
2019-03-05 19:13:17 -0800

    added buttons functions to Mainmenu

--- app/src/main/java/edu/fandm/rkienzle/wordlygame/MainMenu.java
+++ app/src/main/java/edu/fandm/rkienzle/wordlygame/MainMenu.java
@@ -1,7 +1,9 @@
 package edu.fandm.rkienzle.wordlygame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+    public void GoEasy(View v)
+    {
+        Intent intent = new Intent(this, Game.class);
+        startActivity(intent);
+    }
+    public void GoNormal(View v)
+    {
+        Intent intent = new Intent(this, Game.class);
+        startActivity(intent);
+    }
+    public void GoHard(View v)
+    {
+        Intent intent = new Intent(this, Game.class);
+        startActivity(intent);
+    }
+    public void GoCustom(View v)
+    {
+        Intent intent = new Intent(this, Game.class);
+        startActivity(intent);
     }
 }
