19ad863efe6004d034b54971aecfef9df8cf061b
Lam Nguyen <oregon597@gmail.com>
2019-03-06 10:36:30 -0800

    changed backgrounds
    with '#' will be ignored, and an empty message aborts the commit.

--- app/src/main/java/edu/fandm/rkienzle/wordlygame/MainMenu.java
+++ app/src/main/java/edu/fandm/rkienzle/wordlygame/MainMenu.java
@@ -1,16 +1,23 @@
 package edu.fandm.rkienzle.wordlygame;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainMenu extends AppCompatActivity {
+    TextView t;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        t = (TextView) findViewById(R.id.Choose);
     }
     public void GoEasy(View v)
     {
