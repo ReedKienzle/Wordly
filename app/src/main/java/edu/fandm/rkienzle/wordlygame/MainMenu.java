964c2993a2052b477e356203984e861f8b0476b3
Lam Nguyen <oregon597@gmail.com>
2019-03-07 15:49:24 -0800

    Added music, changed button colours, layouts, changed icon, added title screen

--- app/src/main/java/edu/fandm/rkienzle/wordlygame/MainMenu.java
+++ app/src/main/java/edu/fandm/rkienzle/wordlygame/MainMenu.java
@@ -2,6 +2,7 @@ package edu.fandm.rkienzle.wordlygame;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,12 +13,16 @@ import org.w3c.dom.Text;
 public class MainMenu extends AppCompatActivity {
     TextView t;
     String difficulty;
+    MediaPlayer mysong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         t = (TextView) findViewById(R.id.Choose);
+        mysong = MediaPlayer.create(this,R.raw.music);
+        mysong.start();
+        mysong.setLooping(true);
     }
     public void showExplain(View v){
         startActivity(new Intent(this, Explain.class));
